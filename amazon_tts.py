4f56217b61dece90767b922f47151110117d4623
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2018-12-17 17:31:32 +0530

    Now this client can stream 16 English accent using amazon polly service

--- amazon_tts.py
+++ amazon_tts.py
@@ -6,6 +6,7 @@ import os
 import sys
 import subprocess
 from tempfile import gettempdir
+from time import sleep
 # Create a client using the credentials and region defined in the [adminuser]
 # section of the AWS credentials file (~/.aws/credentials).
 session = Session(profile_name="amzonPollyDev")
@@ -22,49 +23,54 @@ language = ['Geraint', # Welsh English - Male
             'Joey', #US English -  Male        
             'Joanna', #US English -  Female  
             'Ivy', #US English -  Female       
-            'Raveena', #Indian English -  Female         
+            'Raveena',  # Indian English -  Female
             'Aditi', #Indian English -  Female         
             'Emma', #British English -Female         
             'Brian',  #British English - Male  
             'Amy', #British English - Female       
-            'Russell', #UAustralian English - Male                   
+            'Russell',  # UAustralian English - Male
             'Nicole' #UAustralian English - Female               
 ]
 
-try:
-    # Request speech synthesis
-    response = polly.synthesize_speech(Text="Parking availble in this floor!", OutputFormat="mp3", VoiceId="Vitoria")
-except (BotoCoreError, ClientError) as error:
-# The service returned an error, exit gracefully
-    print(error)
-    sys.exit(-1)
+# $extension = "mp3";
+for accent in language:
+    try:
+        # Request speech synthesis
+        response = polly.synthesize_speech(Text="Parking availble in this floor!", OutputFormat="mp3", VoiceId=accent)
+    except (BotoCoreError, ClientError) as error:
+        # The service returned an error, exit gracefully
+        print(error)
+        sys.exit(-1)
 
-# Access the audio stream from the response
-if "AudioStream" in response:
-# Note: Closing the stream is important as the service throttles on the
-# number of parallel connections. Here we are using contextlib.closing to
-# ensure the close method of the stream object will be called automatically
-# at the end of the with statement's scope.
-    with closing(response["AudioStream"]) as stream:
-        output = os.path.join(gettempdir(), "speech.mp3")
-	print("File path", output)
-        try:
-        # Open a file for writing the output as a binary stream
-            with open(output, "wb") as file:
-                file.write(stream.read())
-        except IOError as error:
-            # Could not write to file, exit gracefully
-            print(error)
-            sys.exit(-1)
-else:
-    # The response didn't contain audio data, exit gracefully
-    print("Could not stream audio")
-    sys.exit(-1)
+    # Access the audio stream from the response
+    if "AudioStream" in response:
+    # Note: Closing the stream is important as the service throttles on the
+    # number of parallel connections. Here we are using contextlib.closing to   
+    # ensure the close method of the stream object will be called automatically
+    # at the end of the with statement's scope.
+        with closing(response["AudioStream"]) as stream:
+            #output = os.path.join(gettempdir(), "speech.mp3")
+            filename = "name" + ".mp3"
+            output = os.path.join(gettempdir(), filename)
+	    print("File path", output)
+            try:
+            # Open a file for writing the output as a binary stream
+                with open(output, "wb") as file:
+                    file.write(stream.read())
+            except IOError as error:
+                # Could not write to file, exit gracefully
+                print(error)
+                sys.exit(-1)
+    else:
+        # The response didn't contain audio data, exit gracefully
+        print("Could not stream audio")
+        sys.exit(-1)
 
-# Play the audio using the platform's default player
-if sys.platform == "win32":
-    os.startfile(output)
-else:
-    # the following works on Mac and Linux. (Darwin = mac, xdg-open = linux).
-    opener = "open" if sys.platform == "darwin" else "xdg-open"
-    subprocess.call([opener, output])
+    # Play the audio using the platform's default player
+    if sys.platform == "win32":
+        os.startfile(output)
+    else:
+        # the following works on Mac and Linux. (Darwin = mac, xdg-open = linux).
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, output])
+        sleep(2)
