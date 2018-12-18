caee4cd43f8f9489d09ad5c68804010788ee045a
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2018-12-18 12:35:46 +0530

    Added Python script - Excel to Txt conversion

---
+++ excel2txt.py
@@ -0,0 +1,43 @@
+import argparse
+import os
+import re
+import xlrd 
+from xlrd.sheet import ctype_text 
+from os.path import join, dirname, abspath
+import re
+from os.path import expanduser
+
+
+# parser = argparse.ArgumentParser(description='Process some integers.')
+# parser.add_argument('input', metavar='Excel File', type=str,
+#                     help='the Excel input file')
+# args = parser.parse_args()
+
+def urlify(s):
+
+    # Remove all non-word characters (everything except numbers and letters)
+    s = re.sub(r"[^\w\s]", '', s)
+
+    # Replace all runs of whitespace with a single dash
+    s = re.sub(r"\s+", '-', s)
+
+    return s
+
+fname = join(dirname(dirname(abspath(__file__))), 'amazon-polly-TTS', 'textDataset.xlsx')
+xl_workbook = xlrd.open_workbook(fname)
+xl_sheet = xl_workbook.sheet_by_index(0)
+numOfRows = xl_sheet.nrows - 1
+home = expanduser("~")
+folderPath = home + "/" + "textfaPolly"
+if not os.path.exists(folderPath):
+    os.makedirs(folderPath)
+
+for rows in range(0, numOfRows):
+    txt = xl_sheet.cell_value(rows, 0)
+    filename  = urlify(txt) + ".txt"
+    desPath = folderPath + "/" + filename
+    with open(desPath, 'w+') as my_file:
+        print xl_sheet.cell_value(rows, 0)
+        my_file.write(xl_sheet.cell_value(rows, 0))
+
+exit(folderPath) 
