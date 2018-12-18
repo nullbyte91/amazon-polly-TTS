e0841c738e4408a82d283523674ddb74ba500d3f
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2018-12-18 14:44:41 +0530

    Automation script supports all other possible speech rates are x-slow, medium, fast, and x-fast

--- auto.sh
+++ auto.sh
@@ -9,7 +9,19 @@ do
      echo $va$entry 
      filename=$(basename -- $entry)
      var=${filename%%.*}
-     export FILENAME=$DownloadDir$var
+     export FILENAME=$DownloadDir$var-"x-slow-"
+     ./txt2ssml.pl -s x-slow $entry > $va$var
+     python amazon_tts.py $va$var
+     export FILENAME=$DownloadDir$var-"slow-"
      ./txt2ssml.pl -s slow $entry > $va$var
+     python amazon_tts.py $va$var
+     export FILENAME=$DownloadDir$var-"medium-"
+     ./txt2ssml.pl -s medium $entry > $va$var
+     python amazon_tts.py $va$var
+     export FILENAME=$DownloadDir$var-"fast-"
+     ./txt2ssml.pl -s fast $entry > $va$var
+     python amazon_tts.py $va$var
+     export FILENAME=$DownloadDir$var-"x-fast-"
+     ./txt2ssml.pl -s x-fast $entry > $va$var
      python amazon_tts.py $va$var
  done                                                                                            
