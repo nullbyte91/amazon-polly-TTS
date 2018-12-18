1dd241814389c957ee61ec39be70b7b183249dfc
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2018-12-18 17:27:09 +0530

    Download dir path changes and count the character parsing to amazon cloud and print the count

--- auto.sh
+++ auto.sh
@@ -4,24 +4,37 @@
 #generateTxtFile and get a abs path
 va=$(python excel2txt.py textDataset.xlsx 2>&1)
 DownloadDir="/home/nullbyte/DataSet/"
+count=0
+accent=17
 for entry in $va/*;        
 do
      echo $va$entry 
      filename=$(basename -- $entry)
      var=${filename%%.*}
-     export FILENAME=$DownloadDir$var-"x-slow-"
+     mkdir -p $DownloadDir$var
+     count=`expr "$count" + "${#var}" '*' "$accent"`
+     echo $count
+     export FILENAME=$DownloadDir$var"/"$var-"x-slow-"
      ./txt2ssml.pl -s x-slow $entry > $va$var
      python amazon_tts.py $va$var
-     export FILENAME=$DownloadDir$var-"slow-"
+     count=`expr "$count" + "${#var}" '*' "$accent"`
+     echo $count
+     export FILENAME=$DownloadDir$var"/"$var-"slow-"
      ./txt2ssml.pl -s slow $entry > $va$var
      python amazon_tts.py $va$var
-     export FILENAME=$DownloadDir$var-"medium-"
+     count=`expr "$count" + "${#var}" '*' "$accent"`
+     echo $count
+     export FILENAME=$DownloadDir$var"/"$var-"medium-"
      ./txt2ssml.pl -s medium $entry > $va$var
      python amazon_tts.py $va$var
-     export FILENAME=$DownloadDir$var-"fast-"
+     count=`expr "$count" + "${#var}" '*' "$accent"`
+     echo $count
+     export FILENAME=$DownloadDir$var"/"$var-"fast-"
      ./txt2ssml.pl -s fast $entry > $va$var
      python amazon_tts.py $va$var
-     export FILENAME=$DownloadDir$var-"x-fast-"
+     count=`expr "$count" + "${#var}" '*' "$accent"`
+     echo $count
+     export FILENAME=$DownloadDir$var"/"$var-"x-fast-"
      ./txt2ssml.pl -s x-fast $entry > $va$var
      python amazon_tts.py $va$var
  done                                                                                            
