190e438dda9db7226744c91ea78ed01b1de5d042
nullbyte91 <nullbyte.in@gmail.com>
2018-12-21 12:45:36 +0530

    basic_setup script added: This script setup environment for this application

---
+++ basic_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+gStreamer="libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good 
+            gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc 
+            gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 
+            gstreamer1.0-qt5 gstreamer1.0-pulseaudio"
+#MainStarts Here
+systemBasicUpdate
+
+function installrequirements()
+{
+    pip install -r requirements.txt --user
+}
+function systemBasicUpdate()
+{
+	echo "#### Basic ubuntu update"
+	# Update the apt package index and Upgrade the Ubuntu system
+	sudo apt-get update && sudo apt-get -y upgrade
+
+	#Install Git
+	echo "#### Install gStreamer to play audio"
+	for pkg in $gStreamer; do
+        	if dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
+                	echo -e "$pkg is already installed"
+		else
+			if sudo apt-get -qq install $pkg; then
+    				echo "Successfully installed $pkg"
+			else
+    				echo "Error installing $pkg"
+			fi
+		fi
+	done
+}
+
+#MainStarts 
+systemBasicUpdate
+installrequirements
